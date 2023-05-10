b6f755d8f4cac0f7a2bcfe3efc2d6dd48b022386
Minxuan Qin <minqin@eu-login-42.euler.ethz.ch>
2023-05-10 11:07:38 +0200

    final version of gathering, can set N_apples, N_tagged, blood in constants.py

--- test_code/test_gathering.py
+++ test_code/test_gathering.py
@@ -1,3 +1,4 @@
+from builtins import breakpoint
 import os, sys
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
@@ -14,23 +15,33 @@ def rgb_onestep(env, action, step_number):
     env.step(dict(zip(env.agents, action)))
     output = env.grid_world()
     rgb = ascii_array_to_rgb_array(output, env.ascii_color_array)
-    img = Image.fromarray(env.render()).save(f"gathering_imgs/sample{step_number}.png")
+    img = Image.fromarray(env.render()).save(f"gathering_small_map/sample{step_number}.png")
     return env
 
-rng, _ = seeding.np_random(1145)
+## seed 1145 for sample imgs in gathering_imgs
+rng, _ = seeding.np_random(189)
 a = Gathering(rng, )
 a.render_mode = 'rgb_array'
 a.reset()
 #print(ascii_array_to_str(a.grid_world()))
-img = Image.fromarray(a.render()).save('sample.png')
+img = Image.fromarray(a.render()).save('gathering_small_map/sample.png')
 
 # np.random.choice(7, size=(len(a.agents),)).tolist()
-a = rgb_onestep(a, (7,4), 1)
-a = rgb_onestep(a, (0,2), 2)
+a = rgb_onestep(a, (2,5), 1)
+a = rgb_onestep(a, (5,0), 2)
 a = rgb_onestep(a, (0,0), 3)
-a = rgb_onestep(a, (3,7), 4)
-a = rgb_onestep(a, (3,5), 5)
-a = rgb_onestep(a, (1,2), 6)
+a = rgb_onestep(a, (7,0), 4)
+a = rgb_onestep(a, (0,0), 5)
+a = rgb_onestep(a, (0,0), 6)
+a = rgb_onestep(a, (0,0), 7)
+a = rgb_onestep(a, (7,0), 8)
+a = rgb_onestep(a, (2,4), 9)
+a = rgb_onestep(a, (0,4), 10)
+a = rgb_onestep(a, (2,1), 11)
+a = rgb_onestep(a, (0,3), 12)
+a = rgb_onestep(a, (0,3), 13)
+a = rgb_onestep(a, (2,4), 14)
+
 
 from pettingzoo.test import parallel_api_test
 from pettingzoo.butterfly import cooperative_pong_v5
